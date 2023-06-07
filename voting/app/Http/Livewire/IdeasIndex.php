bf2aa72219b71b9702f8803a321483be1c113a11
ansdaultana <116364572+ansdaultana@users.noreply.github.com>
2023-06-07 18:34:35 +0500

    new

--- voting/app/Http/Livewire/IdeasIndex.php
+++ voting/app/Http/Livewire/IdeasIndex.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
 use App\Models\Vote;
@@ -11,7 +12,7 @@ use Livewire\WithPagination;
 class IdeasIndex extends Component
 {
 
-    // use WithPagination;
+    use WithPagination;
     public $status;
     public $category;
 
@@ -22,45 +23,50 @@ class IdeasIndex extends Component
 
     protected $listeners = ['queryStringUpdatedStatus'];
 
-    public function mount()
-    {
-        $this->status =  $this->status ?? 'All';
-    }
-
     public function updatingCategory()
     {
-     //   $this->resetPage();
+        $this->resetPage();
     }
+
+    public function mount()
+    {
+        $this->status = $this->status ?? 'All';
+    }
+
+
 
     public function queryStringUpdatedStatus($newStatus)
     {
-       // $this->resetPage();
+        $this->resetPage();
         $this->status = $newStatus;
     }
 
     public function render()
     {
-
         $statuses = Status::all()->pluck('id', 'name');
-
-        return view('livewire.ideas-index', [
-            "ideas" => Idea::with("user", "category", "status")
-                //checking status filters in search query add by statusfilters
-                ->when( $this->status &&  $this->status !== 'All', function ($query) use ($statuses) {
-                    return $query->where('status_id', $statuses->get( $this->status));
-                })
-                //subquery for finding if user has voted for each idea while extracting all ideas
-                ->addSelect([
-                    'voted_by_user' => Vote::select('id')
-                        ->where('user_id', auth()->id())
-                        ->whereColumn('idea_id', 'ideas.id')
-                ])
-                //now it will look for method votes
-                //which are in votes table
-                //so it will use eloquent relationship automaatically
-                ->withCount('votes')
-                ->orderBy("created_at", "desc")
-                ->Paginate(Idea::PAGINATE_COUNT)
-        ]);
+        $categories = Category::all();
+        return view(
+            'livewire.ideas-index',
+            [
+                "ideas" => Idea::with("user", "category", "status")
+                    //checking status filters in search query add by statusfilters
+                    ->when($this->status && $this->status !== 'All', function ($query) use ($statuses) {
+                        return $query->where('status_id', $statuses->get($this->status));
+                    })
+                    //subquery for finding if user has voted for each idea while extracting all ideas
+                    ->addSelect([
+                        'voted_by_user' => Vote::select('id')
+                            ->where('user_id', auth()->id())
+                            ->whereColumn('idea_id', 'ideas.id')
+                    ])
+                    //now it will look for method votes
+                    //which are in votes table
+                    //so it will use eloquent relationship automaatically
+                    ->withCount('votes')
+                    ->orderBy("created_at", "desc")
+                    ->Paginate(Idea::PAGINATE_COUNT),
+                'categories' => $categories
+            ]
+        );
     }
 }
